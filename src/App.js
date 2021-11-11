c83c9fa5ba08dee71eb7845f6bd1a229197410f8
Paulo Toledo <ptoledo.bsices@gmail.com>
2021-11-10 22:35:09 -0300

    Add styled-components, styled-reset, theme, and types to texts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
 
 import Home from './pages/Home';
+import theme from './theme';
 
 function App() {
-  return <Home />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Reset />
+      <Home />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Text } from '../../components';
 
-const Home = () => <div>Hello world!</div>;
+const Home = () => (
+  <Text size="large" textStyle="uppercase">
+    Hello World!
+  </Text>
+);
 
 export default Home;
